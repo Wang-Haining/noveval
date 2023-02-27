da1ea213f65f01eccab3e1d2be8b7252aa3991ed
Haining Wang <hw56@login1.hsn0.cm.bigred200.uits.iu.edu>
2023-02-26 19:05:23 -0500

    init

---
+++ config/train_gpt2_wikipedia_en.py
@@ -0,0 +1,29 @@
+# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
+# launch as the following (e.g. in a screen session) and wait ~5 days:
+# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
+
+wandb_log = True
+wandb_project = 'ppl_nanogpt'
+wandb_run_name='gpt2-124M'
+
+# these make the total batch size be ~0.33M
+# 16 batch size * 1024 block size * 5 gradaccum * 4 GPUs = 327,680
+batch_size = 16
+block_size = 1024
+dataset = 'wikipedia_en'
+
+# it takes 14,176 iters to exhaust one epoch of wikipedia_en
+# this makes total number of tokens be 300B
+max_iters = 141760
+lr_decay_iters = 141760
+
+# eval stuff
+eval_interval = 1000
+eval_iters = 200
+log_interval = 10
+
+# weight decay
+weight_decay = 1e-1
+
+# compile
+compile = False
---
+++ data/wikipedia_en/readme.md
@@ -0,0 +1,14 @@
+
+## openwebtext dataset
+
+after running `prepare.py` (preprocess) we get:
+
+- train.bin is ~8.7GB, val.bin ~4.3MB
+- train has ~4.6B tokens (4,645,199,244)
+- val has ~2.2M tokens (2,231,052)
+
+this came from 6,458,670 documents in total.
+
+references:
+
+- https://huggingface.co/datasets/wikipedia
