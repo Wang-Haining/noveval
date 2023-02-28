34a6cbfb152aa1580e2177af79983fffd4c937b5
Haining Wang <whn395436129@gmail.com>
2023-02-27 23:51:17 -0700

    change: recalculate iters in config and rename sbatch scripts

--- config/train_gpt2_wikipedia_en_tempest.py
+++ config/train_gpt2_wikipedia_en_tempest.py
@@ -4,18 +4,19 @@
 
 wandb_log = True
 wandb_project = 'ppl_nanogpt'
-wandb_run_name='gpt2-124M'
+wandb_run_name='gpt2-124m-tempest-2A40'
 
-# these make the total batch size be ~0.33M
-# 16 batch size * 1024 block size * 5 gradaccum * 4 GPUs = 327,680
+# these make the total batch size be ~0.16M
+# 16 batch size * 1024 block size * 5 gradaccum * 2 GPUs = 163,840
 batch_size = 16
 block_size = 1024
 dataset = 'wikipedia_en'
 
-# it takes 14,176 iters to exhaust one epoch of wikipedia_en
-# this makes total number of tokens be 300B
-max_iters = 141760
-lr_decay_iters = 141760
+# it takes 28,352 iters to exhaust one epoch of wikipedia_en (of 4,645,199,244 tokens)
+# we run 10 epochs of the training set
+# this makes total number of tokens be 46B 
+max_iters = 283520
+lr_decay_iters = 283520
 
 # eval stuff
 eval_interval = 1000
@@ -25,5 +26,3 @@ log_interval = 10
 # weight decay
 weight_decay = 1e-1
 
-# compile
-# compile = False
