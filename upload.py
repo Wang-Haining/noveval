9aa978186539f524a17b4be3a97790f3023ea054
Haining Wang <hw56@login1.hsn0.cm.bigred200.uits.iu.edu>
2023-03-02 20:46:18 -0500

    update: finished first exp (config/train_gpt2_wikipedia_en_br200.py) and ready to release the ckpt

---
+++ upload.py
@@ -0,0 +1,19 @@
+import os 
+import torch
+from model import GPTConfig, GPT
+
+out_dir = 'out'
+device = 'cpu'
+
+# init from a model saved in a specific directory
+ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+checkpoint = torch.load(ckpt_path, map_location=device)
+gptconf = GPTConfig(**checkpoint['model_args'])
+model = GPT(gptconf)
+state_dict = checkpoint['model']
+unwanted_prefix = '_orig_mod.'
+for k,v in list(state_dict.items()):
+    if k.startswith(unwanted_prefix):
+        state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+model.load_state_dict(state_dict)
+print("ok")
