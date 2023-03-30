d817bc6dd2484b006b5493996c154be60dfe6ce2
hw@monon <whn395436129@gmail.com>
2023-03-29 22:04:13 -0400

    Update config and sbatch file to run arxiv on two A100s on tempest

--- config/train_gpt2_arxiv.py
+++ config/train_gpt2_arxiv.py
@@ -4,21 +4,20 @@
 
 wandb_log = True
 wandb_project = 'ppl_nanogpt'
-wandb_run_name='gpt2-124m-br200-1A100-arxiv'
+wandb_run_name='gpt2-124m-br200-2A100-arxiv'
 init_from = 'resume'
 
 # these make the total batch size be ~0.08M
-# 16 batch size * 1024 block size * 5 gradaccum * 1 GPU = 81,920
+# 16 batch size * 1024 block size * 5 gradaccum * 2 GPU = 163,480
 batch_size = 16
 block_size = 1024
 dataset = 'arxiv'
 
-# it takes ~635 iters to exhaust one epoch of arxiv (of 52,041,540 tokens)
+# it takes ~318 iters to exhaust one epoch of arxiv (of 52,041,540 tokens)
 # we run 10 epochs of training
 # this makes total number of tokens be .5B
-warmup_iters = 500
-max_iters = 141000 + 6350  # 141000 is the total steps of the pretrained gpt2
-lr_decay_iters = 6350
+max_iters = 141000 + 3200  # 141000 is the total steps of the pretrained gpt2
+lr_decay_iters = 141000 + 3200
 
 # eval stuff
 eval_interval = 500
