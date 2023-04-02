0dc0170994108df1275c5254419cd08c369a428b
hw@monon <whn395436129@gmail.com>
2023-04-02 01:24:52 -0400

    Add bash scripts to calcualte ppl

---
+++ run/calculate_ppl.sh
@@ -0,0 +1,23 @@
+python3 calculate_acl_correlation.py \
+--out_dir out_wikipedia_en \
+--device cuda:0 \
+--ppl_computing_method long_history \
+--ignore_function_words
+
+python3 calculate_acl_correlation.py \
+--out_dir out_wikipedia_en \
+--device cuda:0 \
+--ppl_computing_method long_history \
+--no-ignore_function_words
+
+python3 calculate_acl_correlation.py \
+--out_dir out_wikipedia_en \
+--device cuda:0 \
+--ppl_computing_method naive \
+--ignore_function_words
+
+python3 calculate_acl_correlation.py \
+--out_dir out_wikipedia_en \
+--device cuda:0 \
+--ppl_computing_method naive \
+--no-ignore_function_words
