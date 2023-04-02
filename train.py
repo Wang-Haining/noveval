9d8255b73340c58d0e669abd9005c496d9d2849a
hw@monon <whn395436129@gmail.com>
2023-04-02 18:46:05 -0400

    Disable flash attention for training on owt

--- train.py
+++ train.py
@@ -74,11 +74,15 @@ backend = 'nccl' # 'nccl', 'gloo', etc.
 device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
 dtype = 'bfloat16' # 'float32', 'bfloat16', or 'float16', the latter will auto implement a GradScaler
 compile = True # use PyTorch 2.0 to compile the model to be faster
+flash_attention = True
 # -----------------------------------------------------------------------------
 config_keys = [k for k,v in globals().items() if not k.startswith('_') and isinstance(v, (int, float, bool, str))]
 exec(open('configurator.py').read()) # overrides from command line or config file
 config = {k: globals()[k] for k in config_keys} # will be useful for logging
 # -----------------------------------------------------------------------------
+# disable flash attention as suggested in https://github.com/karpathy/nanoGPT/issues/137
+if not flash_attention:
+    from model_disable_flash_attention import GPT
 
 # various inits, derived attributes, I/O setup
 ddp = int(os.environ.get('RANK', -1)) != -1 # is this a ddp run?
