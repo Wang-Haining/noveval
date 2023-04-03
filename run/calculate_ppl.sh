ea2a1fab511f044088650547199ec0df67630485
hw@monon <whn395436129@gmail.com>
2023-04-02 22:26:28 -0400

    Calcualte ppl/ttr with right config and utilities

--- run/calculate_ppl.sh
+++ run/calculate_ppl.sh
@@ -1,22 +1,22 @@
-python3 calculate_acl_correlation.py \
+python3 calculate_ppl_acl.py \
 --out_dir out_wikipedia_en \
 --device cuda:0 \
 --ppl_computing_method long_history \
 --ignore_function_words
 
-python3 calculate_acl_correlation.py \
+python3 calculate_ppl_acl.py \
 --out_dir out_wikipedia_en \
 --device cuda:0 \
 --ppl_computing_method long_history \
 --no-ignore_function_words
 
-python3 calculate_acl_correlation.py \
+python3 calculate_ppl_acl.py \
 --out_dir out_wikipedia_en \
 --device cuda:0 \
 --ppl_computing_method naive \
 --ignore_function_words
 
-python3 calculate_acl_correlation.py \
+python3 calculate_ppl_acl.py \
 --out_dir out_wikipedia_en \
 --device cuda:0 \
 --ppl_computing_method naive \
