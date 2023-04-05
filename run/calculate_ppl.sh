43fcc8d3a205a41c3d3758e4806bdb157a53792e
hw@monon <whn395436129@gmail.com>
2023-04-04 22:03:28 -0400

    calculate ppl with ckpt trained on openwebtext

--- run/calculate_ppl.sh
+++ run/calculate_ppl.sh
@@ -21,3 +21,27 @@ python3 calculate_ppl_acl.py \
 --device cuda:0 \
 --ppl_computing_method naive \
 --no-ignore_function_words
+
+python3 calculate_ppl_acl.py \
+--out_dir out_openwebtext \
+--device cuda:0 \
+--ppl_computing_method long_history \
+--ignore_function_words
+
+python3 calculate_ppl_acl.py \
+--out_dir out_openwebtext \
+--device cuda:0 \
+--ppl_computing_method long_history \
+--no-ignore_function_words
+
+python3 calculate_ppl_acl.py \
+--out_dir out_openwebtext \
+--device cuda:0 \
+--ppl_computing_method naive \
+--ignore_function_words
+
+python3 calculate_ppl_acl.py \
+--out_dir out_openwebtext \
+--device cuda:0 \
+--ppl_computing_method naive \
+--no-ignore_function_words
