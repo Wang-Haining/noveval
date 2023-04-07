62584732d1db0149c44b0500ed76f41e9a2462b0
hw@monon <whn395436129@gmail.com>
2023-04-06 23:55:45 -0400

    Add right exp

--- run/calculate_ppl.sh
+++ run/calculate_ppl.sh
@@ -1,25 +1,21 @@
 python3 calculate_ppl_acl.py \
 --model_dir out_wikipedia_en \
 --device cuda:0 \
+--mimimum_context_length 512 \
 --computing_method long_history \
 
-#python3 calculate_ppl_acl.py \
-#--model_dir out_wikipedia_en \
-#--device cuda:0 \
-#--mimimum_context_length 1023 \
-#--computing_method long_history \
-#
-#python3 calculate_ppl_acl.py \
-#--model_dir out_wikipedia_en \
-#--device cuda:0 \
-#--computing_method long_history \
-#
-#python3 calculate_ppl_acl.py \
-#--model_dir out_wikipedia_en \
-#--device cuda:0 \
-#--computing_method naive \
-#
-#python3 calculate_ppl_acl.py \
-#--model_dir out_openwebtext \
-#--device cuda:0 \
-#--computing_method long_history \
+python3 calculate_ppl_acl.py \
+--model_dir out_wikipedia_en \
+--device cuda:0 \
+--mimimum_context_length 1023 \
+--computing_method long_history \
+
+python3 calculate_ppl_acl.py \
+--model_dir out_wikipedia_en \
+--device cuda:0 \
+--computing_method naive \
+
+python3 calculate_ppl_acl.py \
+--model_dir out_openwebtext \
+--device cuda:0 \
+--computing_method long_history \
