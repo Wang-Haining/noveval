a52eefe8284b99085b8a6e2995fe97ba434babd8
hw@monon <whn395436129@gmail.com>
2024-01-03 00:43:10 -0500

    style: improve code style

--- utils.py
+++ utils.py
@@ -3,26 +3,28 @@ This script provides utilities useful in producing the findings in the paper 'A 
 Publications Aligned with Peer Review'.
 """
 
+import json
+import math
 import os
 import re
-import math
-import json
-import torch
 import warnings
-import tiktoken
-import matplotlib
-import numpy as np
-from model import GPT
-import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
-from typing import List, Dict, Tuple, Optional
+from typing import Dict, List, Optional, Tuple
 
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import tiktoken
+import torch
+from sklearn.preprocessing import MinMaxScaler
+
+from model import GPT
 
 __author__ = "hw56@indiana.edu"
 
 
-def prepare_acl2017_corpus(corpus_path: str = "./PeerRead/data/acl_2017/",
-                           preserve_ordinal: bool = True) -> Dict[str, List]:
+def prepare_acl2017_corpus(
+    corpus_path: str = "./PeerRead/data/acl_2017/", preserve_ordinal: bool = True
+) -> Dict[str, List]:
     """
     Prepare ACL2017 papers and novelty ratings from ACL2017 subset of PeerRead.
 
@@ -44,9 +46,9 @@ def prepare_acl2017_corpus(corpus_path: str = "./PeerRead/data/acl_2017/",
         """Round up mean score to the nearest whole number for ordinal preservation."""
         return int(math.ceil(mean_score - 0.5))
 
-    def get_review_aspect_score(reviews: List[Dict],
-                                aspect: str,
-                                preserve_ordinal: bool) -> Tuple[List[int], List[int]]:
+    def get_review_aspect_score(
+        reviews: List[Dict], aspect: str, preserve_ordinal: bool
+    ) -> Tuple[List[int], List[int]]:
         """
         Extract and process aspect scores from reviews.
 
@@ -59,42 +61,83 @@ def prepare_acl2017_corpus(corpus_path: str = "./PeerRead/data/acl_2017/",
             sorted lists of aspect scores and corresponding paper indices.
         """
         if not preserve_ordinal:
-            aspect_dict = [{int(r['id']): np.mean([int(d[aspect]) for d in r['reviews']])} for r in reviews]
+            aspect_dict = [
+                {int(r["id"]): np.mean([int(d[aspect]) for d in r["reviews"]])}
+                for r in reviews
+            ]
         else:
-            aspect_dict = [{int(r['id']): round_up(np.mean([int(d[aspect]) for d in r['reviews']]))} for r in reviews]
-        aspect_sorted_list = [list(d.values())[0] for d in sorted(aspect_dict, key=lambda x: list(x.keys())[0])]
-        aspect_index_list = [list(d.keys())[0] for d in sorted(aspect_dict, key=lambda x: list(x.keys())[0])]
+            aspect_dict = [
+                {
+                    int(r["id"]): round_up(
+                        np.mean([int(d[aspect]) for d in r["reviews"]])
+                    )
+                }
+                for r in reviews
+            ]
+        aspect_sorted_list = [
+            list(d.values())[0]
+            for d in sorted(aspect_dict, key=lambda x: list(x.keys())[0])
+        ]
+        aspect_index_list = [
+            list(d.keys())[0]
+            for d in sorted(aspect_dict, key=lambda x: list(x.keys())[0])
+        ]
         return aspect_sorted_list, aspect_index_list
 
     # define paths for paper and review data
-    split_paths = [os.path.join(corpus_path, s) for s in ['train', 'dev', 'test']]
-    paper_paths = [p.path for s in split_paths for p in os.scandir(os.path.join(s, 'parsed_pdfs')) if
-                   p.path.endswith('.json')]
-    review_paths = [p.path for s in split_paths for p in os.scandir(os.path.join(s, 'reviews')) if
-                    p.path.endswith('.json')]
+    split_paths = [os.path.join(corpus_path, s) for s in ["train", "dev", "test"]]
+    paper_paths = [
+        p.path
+        for s in split_paths
+        for p in os.scandir(os.path.join(s, "parsed_pdfs"))
+        if p.path.endswith(".json")
+    ]
+    review_paths = [
+        p.path
+        for s in split_paths
+        for p in os.scandir(os.path.join(s, "reviews"))
+        if p.path.endswith(".json")
+    ]
 
     # process papers
-    papers = [json.load(open(f, 'r')) for f in paper_paths]
-    text_dict = [{int(paper['name'].split('.pdf')[0]): "\n\n".join(
-        [d['text'] for d in paper['metadata']['sections']])} for paper in papers]
-    title_dict = [{int(paper['name'].split('.pdf')[0]): paper['metadata']['title']} for paper in papers]
-    text_sorted_list = [list(d.values())[0] for d in sorted(text_dict, key=lambda x: list(x.keys())[0])]
-    text_check_list = [list(d.keys())[0] for d in sorted(text_dict, key=lambda x: list(x.keys())[0])]
-    title_sorted_list = [list(d.values())[0] for d in sorted(title_dict, key=lambda x: list(x.keys())[0])]
+    papers = [json.load(open(f, "r")) for f in paper_paths]
+    text_dict = [
+        {
+            int(paper["name"].split(".pdf")[0]): "\n\n".join(
+                [d["text"] for d in paper["metadata"]["sections"]]
+            )
+        }
+        for paper in papers
+    ]
+    title_dict = [
+        {int(paper["name"].split(".pdf")[0]): paper["metadata"]["title"]}
+        for paper in papers
+    ]
+    text_sorted_list = [
+        list(d.values())[0] for d in sorted(text_dict, key=lambda x: list(x.keys())[0])
+    ]
+    text_check_list = [
+        list(d.keys())[0] for d in sorted(text_dict, key=lambda x: list(x.keys())[0])
+    ]
+    title_sorted_list = [
+        list(d.values())[0] for d in sorted(title_dict, key=lambda x: list(x.keys())[0])
+    ]
 
     # process reviews
-    reviews = [json.load(open(f, 'r')) for f in review_paths]
+    reviews = [json.load(open(f, "r")) for f in review_paths]
     review_scores = {}
     review_checks = {}
-    score, check = get_review_aspect_score(reviews, 'ORIGINALITY', preserve_ordinal)
-    review_scores.update({'originality': score})
-    review_checks.update({'originality': check})
+    score, check = get_review_aspect_score(reviews, "ORIGINALITY", preserve_ordinal)
+    review_scores.update({"originality": score})
+    review_checks.update({"originality": check})
 
     # check if paper and review indices are aligned
     if not all(v == text_check_list for v in list(review_checks.values())):
-        raise AttributeError("Expect aligned indices between paper and review aspect scores.")
+        raise AttributeError(
+            "Expect aligned indices between paper and review aspect scores."
+        )
 
-    review_scores.update({'paper': text_sorted_list, 'title': title_sorted_list})
+    review_scores.update({"paper": text_sorted_list, "title": title_sorted_list})
 
     return review_scores
 
@@ -134,7 +177,7 @@ def clean_up_artifacts(text_from_parsed_pdf: str) -> str:
     """
     newline_number_pattern = re.compile(r"\n\d+")
     one_thousand_pattern = re.compile(r"1 000\n\n?")
-    long_quotes_pattern = re.compile(r'\"{10,}')
+    long_quotes_pattern = re.compile(r"\"{10,}")
 
     text = newline_number_pattern.sub("", text_from_parsed_pdf)
     text = one_thousand_pattern.sub("", text)
@@ -143,9 +186,9 @@ def clean_up_artifacts(text_from_parsed_pdf: str) -> str:
     return text
 
 
-def get_top_choices_and_ranks(logits: torch.Tensor,
-                              y: List[int],
-                              top_k: int = 30) -> Tuple[List[List[Tuple[str, int, float, float]]], List[int]]:
+def get_top_choices_and_ranks(
+    logits: torch.Tensor, y: List[int], top_k: int = 30
+) -> Tuple[List[List[Tuple[str, int, float, float]]], List[int]]:
     """
     Computes the probabilities from the logits and identifies the top 'k' choices for each token. It returns the top
     choices along with their ranks in the sequence. Each choice includes the token, its index, surprisal score, and
@@ -187,25 +230,31 @@ def get_top_choices_and_ranks(logits: torch.Tensor,
 
         # get the rank of the current token in y
         current_token = y[i]
-        sorted_probs = np.sort(token_probs)[::-1]  # Sort probabilities in descending order
+        sorted_probs = np.sort(token_probs)[
+            ::-1
+        ]  # sort probabilities in descending order
         # find the rank of the current token's probability
-        current_token_rank = np.where(sorted_probs == token_probs[current_token])[0][0] + 1
+        current_token_rank = (
+            np.where(sorted_probs == token_probs[current_token])[0][0] + 1
+        )
         ranks.append(current_token_rank)
 
     return top_choices, ranks
 
 
 @torch.no_grad()
-def calculate_surprisal(text: str,
-                        model: GPT,
-                        context_length: int = 512,
-                        sequence_length: int = 2048,
-                        use_all_tokens: bool = False,
-                        device: str = 'cpu',
-                        top_k: int = 30,
-                        block_size: int = 1024,
-                        random_state: Optional[int] = None,
-                        compile_model: bool = True) -> [np.float64 | Tuple[np.float64, List[np.float64], list, str]]:
+def calculate_surprisal(
+    text: str,
+    model: GPT,
+    context_length: int = 512,
+    sequence_length: int = 2048,
+    use_all_tokens: bool = False,
+    device: str = "cpu",
+    top_k: int = 30,
+    block_size: int = 1024,
+    random_state: Optional[int] = None,
+    compile_model: bool = True,
+) -> [np.float64 | Tuple[np.float64, List[np.float64], list, str]]:
     """
     Calculate the surprisal in bits of a continuous sequence of tokens extracted from a given text. The function can
     operate over a specified number of tokens or the entire text if 'use_all_tokens' is True.
@@ -236,7 +285,9 @@ def calculate_surprisal(text: str,
     """
     # sanity check
     if sequence_length < block_size:  # 1024
-        raise ValueError(f'Expect `sequence_length` longer than 1024, but got {sequence_length}.')
+        raise ValueError(
+            f"Expect `sequence_length` longer than 1024, but got {sequence_length}."
+        )
     # prepare model
     if compile_model:
         model = torch.compile(model)
@@ -250,7 +301,9 @@ def calculate_surprisal(text: str,
     # sanity check
     data_length = len(data)
     if data_length <= sequence_length + context_length:
-        raise ValueError("Data length is too short for the given sequence and context lengths.")
+        raise ValueError(
+            "Data length is too short for the given sequence and context lengths."
+        )
 
     losses = []
     ys = []  # document verbosity output
@@ -259,17 +312,24 @@ def calculate_surprisal(text: str,
     # whether to select a random (fixed-length) section to measure
     if random_state:
         rng = np.random.default_rng(random_state)
-        begin_loc = rng.integers(low=0, high=len(data) - sequence_length - context_length - 1)
+        begin_loc = rng.integers(
+            low=0, high=len(data) - sequence_length - context_length - 1
+        )
     else:
         begin_loc = 0
     # a reasonably long context length can improve surprisal estimation
     if context_length:
         if len(data) < sequence_length + context_length:
             if not use_all_tokens:
-                raise ValueError(f"Input is too short: only {len(data)} tokens provided."
-                                 f"Expected more than {sequence_length + context_length} tokens.")
+                raise ValueError(
+                    f"Input is too short: only {len(data)} tokens provided."
+                    f"Expected more than {sequence_length + context_length} tokens."
+                )
             else:
-                warnings.warn(f'Input is shorter than expected; only {len(data)} tokens are used.', RuntimeWarning)
+                warnings.warn(
+                    f"Input is shorter than expected; only {len(data)} tokens are used.",
+                    RuntimeWarning,
+                )
         total_calculated_tokens = 0
         # Strategy:
         # 1. attempt to move forward by (block_size - context_length) tokens each time
@@ -278,28 +338,38 @@ def calculate_surprisal(text: str,
         step = block_size - context_length
         # if use_all_tokens, sequence_length is only a recommended, minimum length
         total_length = len(data) if use_all_tokens else sequence_length
-        max_iters = total_length // step if not (total_length % step) else total_length // step + 1
+        max_iters = (
+            total_length // step
+            if not (total_length % step)
+            else total_length // step + 1
+        )
         begin_locs = [begin_loc + i * step for i in range(max_iters)]
         for i, begin_loc in enumerate(begin_locs):
             # fit a whole forward pass
             if total_calculated_tokens + block_size <= total_length:
-                x = (data[begin_loc: begin_loc + block_size]).astype(np.int64)
-                y = (data[begin_loc + 1: begin_loc + block_size + 1]).astype(np.int64)
+                x = (data[begin_loc : begin_loc + block_size]).astype(np.int64)
+                y = (data[begin_loc + 1 : begin_loc + block_size + 1]).astype(np.int64)
                 num_covered_tokens = context_length
             # fit a forward pass with at least `context_length` tokens
             # fixme: the logic works but not elegant
-            elif total_calculated_tokens + step <= total_length < total_calculated_tokens + block_size:
+            elif (
+                total_calculated_tokens + step
+                <= total_length
+                < total_calculated_tokens + block_size
+            ):
                 num_covered_tokens = total_length - begin_loc
                 if use_all_tokens:
-                    x = (data[begin_loc: -1]).astype(np.int64)
-                    y = (data[begin_loc + 1:]).astype(np.int64)
+                    x = (data[begin_loc:-1]).astype(np.int64)
+                    y = (data[begin_loc + 1 :]).astype(np.int64)
                 else:
                     if len(data) - 1 >= block_size + begin_loc:
-                        x = (data[begin_loc: begin_loc + block_size]).astype(np.int64)
-                        y = (data[begin_loc + 1:  begin_loc + block_size + 1]).astype(np.int64)
+                        x = (data[begin_loc : begin_loc + block_size]).astype(np.int64)
+                        y = (data[begin_loc + 1 : begin_loc + block_size + 1]).astype(
+                            np.int64
+                        )
                     else:
-                        x = (data[begin_loc: -1]).astype(np.int64)
-                        y = (data[begin_loc + 1:]).astype(np.int64)
+                        x = (data[begin_loc:-1]).astype(np.int64)
+                        y = (data[begin_loc + 1 :]).astype(np.int64)
             else:
                 continue
             x = torch.from_numpy(x).view(1, -1)  # 1, <=1024
@@ -311,7 +381,9 @@ def calculate_surprisal(text: str,
             _loss = loss[num_covered_tokens:]
             # casual lm: x_{:i} -> logits[i,:]-> y_i (i.e., x_{:i+1})
             # fixme: 50304 is only for tiktoken's gpt2 tokenizer
-            _logits = logits.view(-1, 50304)[num_covered_tokens:, :]  # 50304 is the vocab_size
+            _logits = logits.view(-1, 50304)[
+                num_covered_tokens:, :
+            ]  # 50304 is the vocab_size
             _y = y.cpu().numpy()[0][num_covered_tokens:].tolist()  # only for ranking
             # calculate and document results
             _choices, _ranks = get_top_choices_and_ranks(_logits, _y, top_k)
@@ -324,14 +396,20 @@ def calculate_surprisal(text: str,
     # move `block_size` forward every time (only for illustrative purposes, never use in practice)
     else:  # context_length == 0
         if len(data) < sequence_length + 1:
-            raise ValueError(f"Input is too short: only {len(data)} tokens provided."
-                             f"Expected more than {sequence_length + 1} tokens.")
+            raise ValueError(
+                f"Input is too short: only {len(data)} tokens provided."
+                f"Expected more than {sequence_length + 1} tokens."
+            )
         # ignore last tokens do not naturally fit in a forward pass
-        begin_locs = [begin_loc + i * block_size for i in range(sequence_length // block_size)]
+        begin_locs = [
+            begin_loc + i * block_size for i in range(sequence_length // block_size)
+        ]
 
         for begin_loc_tmp in begin_locs:
-            x = (data[begin_loc_tmp: begin_loc_tmp + block_size]).astype(np.int64)
-            y = (data[begin_loc_tmp + 1: begin_loc_tmp + block_size + 1]).astype(np.int64)
+            x = (data[begin_loc_tmp : begin_loc_tmp + block_size]).astype(np.int64)
+            y = (data[begin_loc_tmp + 1 : begin_loc_tmp + block_size + 1]).astype(
+                np.int64
+            )
             ys.extend(y.tolist())
             x, y = torch.from_numpy(x).view(1, -1), torch.from_numpy(y).view(1, -1)
             logits, loss = model.forward_reduction_none(x.to(device), y.to(device))
@@ -344,11 +422,13 @@ def calculate_surprisal(text: str,
             _choices, _ranks = get_top_choices_and_ranks(_logits, _y, top_k)
             choices.extend(_choices)
             ranks.extend(_ranks)
-    return ([loss / np.log(2) for loss in losses],  # 2-based surprisal at each position
-            choices,  # topic candidates at each position
-            ys,  # token ids for the (sampled) sequence used for surprisal calculation
-            ranks,  # ranking of the current token based on its previous prediction
-            decode([y for y in ys if y != -1]))  # sampled sequence for surprisal calculation
+    return (
+        [loss / np.log(2) for loss in losses],  # 2-based surprisal at each position
+        choices,  # topic candidates at each position
+        ys,  # token ids for the (sampled) sequence used for surprisal calculation
+        ranks,  # ranking of the current token based on its previous prediction
+        decode([y for y in ys if y != -1]),
+    )  # sampled sequence for surprisal calculation
 
 
 def decode_ids_for_visualization(ids: List[int]) -> List[str]:
@@ -363,7 +443,10 @@ def decode_ids_for_visualization(ids: List[int]) -> List[str]:
     """
 
     enc = tiktoken.get_encoding("gpt2")
-    return [enc.decode_single_token_bytes(t_id).decode("utf-8", errors='replace') for t_id in ids]
+    return [
+        enc.decode_single_token_bytes(t_id).decode("utf-8", errors="replace")
+        for t_id in ids
+    ]
 
 
 def colorize_text(words: List[str], cross_entropy: List[float]) -> str:
@@ -384,12 +467,16 @@ def colorize_text(words: List[str], cross_entropy: List[float]) -> str:
         a string with each word from the input list colorized based on its scaled cross-entropy value.
     """
     minmax = MinMaxScaler()
-    hot = plt.cm.get_cmap('hot', 256)
-    new_hot = hot(np.linspace(0.25, 1, 192))  # use the 3/4 of the higher end of cmap "hot"
-    cmap = plt.cm.colors.LinearSegmentedColormap.from_list('reversed_hot', new_hot[::-1])
+    hot = plt.cm.get_cmap("hot", 256)
+    new_hot = hot(
+        np.linspace(0.25, 1, 192)
+    )  # use the 3/4 of the higher end of cmap "hot"
+    cmap = plt.cm.colors.LinearSegmentedColormap.from_list(
+        "reversed_hot", new_hot[::-1]
+    )
     color_array = minmax.fit_transform(np.array(cross_entropy).reshape(-1, 1))
     template = '<span class="barcode" title="surp: {:.2f}" style="color: black; background-color: {}">{}</span>'
-    colored_string = ''
+    colored_string = ""
     for word, color, ce in zip(words, color_array, cross_entropy):
         color = matplotlib.colors.rgb2hex(cmap(color[0])[:3])
         colored_string += template.format(ce, color, word)
