3f7f0c405ea35c1df83de7e34c5c6e2c36410a0e
hw@monon <whn395436129@gmail.com>
2024-01-09 22:39:11 -0500

    chore: clean up

--- config/train_gpt2_openwebtext.py
+++ config/train_gpt2_openwebtext.py
@@ -1,20 +1,19 @@
-# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
-# launch as the following (e.g. in a screen session) and wait ~5 days:
-# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
-
 wandb_log = True
-wandb_project = 'ppl_nanogpt'
-wandb_run_name='gpt2-124m-br200-4A100-openwebtext'
+wandb_project = 'noveval'
+wandb_run_name = 'gpt2-124m-br200-4A100-openwebtext'
 
-# these make the total batch size be ~0.33M
+# these make the total batch size ~0.33M
 # 16 batch size * 1024 block size * 5 gradaccum * 4 GPUs = 327,680
 batch_size = 16
 block_size = 1024
+dataset = 'openwebtext'
 gradient_accumulation_steps = 5
 
-# this makes total number of tokens be 300B
-max_iters = 141760
-lr_decay_iters = 141760
+# it takes 14,176 iters to exhaust one epoch of wikipedia_en
+# we reuse the number in order to control of number of tokens seen during training
+# this makes total number of tokens be 46B
+max_iters = 141000
+lr_decay_iters = 141000
 
 # eval stuff
 eval_interval = 1000
--- config/train_gpt2_wikipedia_en.py
+++ config/train_gpt2_wikipedia_en.py
@@ -1,23 +1,19 @@
-# config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
-# launch as the following (e.g. in a screen session) and wait ~5 days:
-# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
-
 wandb_log = True
-wandb_project = 'ppl_nanogpt'
-wandb_run_name='gpt2-124m-br200-4A100-wikipedia_en'
-gradient_accumulation_steps = 5
+wandb_project = 'noveval'
+wandb_run_name = 'gpt2-124m-br200-4A100-wikipedia_en'
 
-# these make the total batch size be ~0.33M
+# these make the total batch size ~0.33M
 # 16 batch size * 1024 block size * 5 gradaccum * 4 GPUs = 327,680
 batch_size = 16
 block_size = 1024
 dataset = 'wikipedia_en'
+gradient_accumulation_steps = 5
 
 # it takes 14,176 iters to exhaust one epoch of wikipedia_en
 # we run 10 epochs of training
 # this makes total number of tokens be 46B
-max_iters = 141760
-lr_decay_iters = 141760
+max_iters = 141000
+lr_decay_iters = 141000
 
 # eval stuff
 eval_interval = 1000
@@ -29,3 +25,6 @@ weight_decay = 1e-1
 
 # no compile, as it is not stable
 compile = False
+
+# adamw eps
+eps = 1e-5
